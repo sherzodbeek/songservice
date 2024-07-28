e6db866a233c6f0ca1faceb85af5258b1fecb1ad
Sherzod Omonov <sherzodbeek98@gmail.com>
2024-07-28 22:54:34 +0500

    contract test fixed

--- src/test/java/com/epam/songservice/contract/BaseTestClass.java
+++ src/test/java/com/epam/songservice/contract/BaseTestClass.java
@@ -1,31 +1,29 @@
 package com.epam.songservice.contract;
 
+import com.epam.songservice.BaseIntegrationTest;
 import com.epam.songservice.controller.SongController;
 import io.restassured.config.EncoderConfig;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.module.mockmvc.config.RestAssuredMockMvcConfig;
-import org.junit.Before;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @DirtiesContext
 @AutoConfigureMessageVerifier
-public class BaseTestClass {
+public class BaseTestClass extends BaseIntegrationTest {
 
     @Autowired
     SongController songController;
 
-    @Before
+    @BeforeEach
     public void setup() {
         StandaloneMockMvcBuilder standaloneMockMvcBuilder
                 = MockMvcBuilders.standaloneSetup(songController);
